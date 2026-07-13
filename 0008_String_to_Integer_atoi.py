20010e444b7eb94605b7fcf4794cdc04d82b2ca4
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-13 21:04:32 +0530

    Find the Index of the First Occurrence using basic string slicing

--- 0008_String_to_Integer_atoi.py
+++ 0008_String_to_Integer_atoi.py
@@ -1,1 +1,25 @@
-0008_String_to_Integer_atoi.py
+#12/07/2026
+#Medium
+# LeetCode 8: Convert a string to an integer (atoi) using sequential pointer parsing.
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        i, n = 0, len(s)
+        while i < n and s[i] == ' ':
+            i += 1
+        sign = 1
+        if i < n and (s[i] == '+' or s[i] == '-'):
+            if s[i] == '-':
+                sign = -1
+            i += 1
+        result = 0
+        while i < n and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1
+        result *= sign
+        INT_MIN, INT_MAX = -2147483648, 2147483647
+        if result < INT_MIN:
+            return INT_MIN
+        elif result > INT_MAX:
+            return INT_MAX
+        return result
