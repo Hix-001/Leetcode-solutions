528242e32c2f37fe10d8a03db54e79f9f3aefb73
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-07-26 22:16:26 +0530

    updated

--- 0136_Single_Number.py
+++ 0136_Single_Number.py
@@ -2,3 +2,9 @@
 # Easy
 # LeetCode 136: Single Number using bitwise XOR to cancel out paired elements.
 
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
