7ea9def7a15bdb040286442b16660f427a631df9
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-12 11:20:55 +0530

    Linked List Cycle using Floyd's Tortoise and Hare

---
+++ 0141_Linked_List_Cycle.py
@@ -0,0 +1,21 @@
+# 12/08/2026
+# Easy
+# LeetCode 141: Linked List Cycle using Floyd's Tortoise and Hare algorithm.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+                
+        return False
