bc87b74964e0eae721cbd115e6166370019de8f4
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-14 23:25:51 +0530

    Hamming Distance using bitwise XOR without OOP

--- 0461_Hamming_Distance.py
+++ 0461_Hamming_Distance.py
@@ -1,1 +1,13 @@
-0461_Hamming_Distance.py
+# 14/08/2026
+# Easy
+# LeetCode 461: Hamming Distance using basic bitwise XOR and shifting.
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor_result = x ^ y
+        distance = 0
+        
+        while xor_result > 0:
+            distance += xor_result & 1
+            xor_result >>= 1
+            
+        return distance
