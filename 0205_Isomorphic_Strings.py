32ea0d7b56eaaad624b59ee3501b8fa6acfe86fd
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-23 19:07:11 +0530

    Isomorphic Strings bidirectional hash map check

---
+++ 0205_Isomorphic_Strings.py
@@ -0,0 +1,16 @@
+# 23/08/2026
+# Easy
+# LeetCode 205: Isomorphic Strings using bidirectional hash maps.
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map_s_t = {}
+        map_t_s = {}
+        
+        for c1, c2 in zip(s, t):
+            if (c1 in map_s_t and map_s_t[c1] != c2) or (c2 in map_t_s and map_t_s[c2] != c1):
+                return False
+            map_s_t[c1] = c2
+            map_t_s[c2] = c1
+            
+        return True
