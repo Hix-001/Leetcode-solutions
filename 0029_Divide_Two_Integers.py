192cd58112f954173d6490127aa69142979fe2be
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-08-31 23:01:26 +0530

    Divide Two Integers using bitwise left shift operators

---
+++ 0029_Divide_Two_Integers.py
@@ -0,0 +1,30 @@
+# 31/08/2026
+# Medium
+# LeetCode 29: Divide Two Integers using bitwise shifts.
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+            
+        is_negative = (dividend < 0) != (divisor < 0)
+        
+        abs_dividend = abs(dividend)
+        abs_divisor = abs(divisor)
+        
+        quotient = 0
+        
+        while abs_dividend >= abs_divisor:
+            temp_divisor = abs_divisor
+            multiple = 1
+            while abs_dividend >= (temp_divisor << 1):
+                temp_divisor <<= 1
+                multiple <<= 1
+                
+            abs_dividend -= temp_divisor
+            quotient += multiple
+            
+        if is_negative:
+            quotient = -quotient
+            
+        return min(max(-2147483648, quotient), 2147483647)
