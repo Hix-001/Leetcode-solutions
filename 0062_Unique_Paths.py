95a3eb659b2291946502934fbac849ca678955df
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-12 17:34:03 +0530

    Unique Paths using space optimized 1D dynamic programming array

---
+++ 0062_Unique_Paths.py
@@ -0,0 +1,15 @@
+# 12/09/2026
+# Medium
+# LeetCode 62: Unique Paths using space-optimized dynamic programming.
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
+        return dp[-1]
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.uniquePaths(3, 7))
+    print(sol.uniquePaths(3, 2))
