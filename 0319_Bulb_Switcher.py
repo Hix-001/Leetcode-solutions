e1a2763a49e3d4bf1796963e5fe900cb22de2607
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-13 18:56:12 +0530

    Bulb Switcher using O(1) perfect square math logic

---
+++ 0319_Bulb_Switcher.py
@@ -0,0 +1,7 @@
+# 13/09/2026
+# Medium
+# LeetCode 319: Bulb Switcher using perfect square mathematical derivation.
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(n ** 0.5)
