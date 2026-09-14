d93ab9c0fa844fb676e8e14d06a857f62c8127e5
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-14 18:27:32 +0530

    Stone Game using O(1) parity strategy optimization

---
+++ 0877_Stone_Game.py
@@ -0,0 +1,11 @@
+# 14/09/2026
+# Medium
+# LeetCode 877: Stone Game using O(1) mathematical game theory proof.
+
+class Solution:
+    def stoneGame(self, piles: list[int]) -> bool:
+        return True
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.stoneGame([5, 3, 4, 5]))
+    print(sol.stoneGame([3, 7, 2, 3]))
