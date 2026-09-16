0bedb3e2fd2d8d03b635086d9ac13c1a41aba6a0
hix-001 <harsh.25scs1003004095@iilm.edu>
2026-09-16 15:41:53 +0530

    Partitioning Into Minimum Number Of Deci-Binary Numbers using max digit approach

---
+++ 1689_Partitioning_Into_Minimum_Number_Of.py
@@ -0,0 +1,7 @@
+# 16/09/2026
+# Medium
+# LeetCode 1689: Partitioning Into Minimum Number Of Deci-Binary Numbers using max digit logic.
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
